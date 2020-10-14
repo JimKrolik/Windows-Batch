1cd09f394ea16aabf56916c881b65438a0686209
James Krolik <53527367+JimKrolik@users.noreply.github.com>
2020-10-14 12:54:23 -0500

    Update MassInstall.bat

--- Mass Install Program/MassInstall.bat
+++ Mass Install Program/MassInstall.bat
@@ -9,6 +9,9 @@ REM	Please review my example of copying a file to all machines for more input.
 REM	I'd recommend looking up the concept of a "For Loop" in programming to get the overall idea, but essentially the first line does the following:
 REM	Creates a For loop that tokenizes (reads every line) of the file and assigns it to the variable %%x in the file B219.txt and then performs everything between the ( and ).
 
+REM	Note:  You will need to download PSEXEC and place PSEXEC.exe into the same directory this is being run from.
+REM	Note:  You will need to download an MSI for deployment and place it in the same directory this is being run from.
+
 REM	PSEXEC can be downloaded from here as of 10/14/2020:	https://docs.microsoft.com/en-us/sysinternals/downloads/psexec
 
 for /F "tokens=*" %%x in (B219.txt) do (
